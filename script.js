32380f88eccfffa6154d088402fee50a2dd16006
kistakki <takacspetra95@gmail.com>
2023-10-17 18:10:31 +0200

    team

--- script.js
+++
@@ -1,5 +0,0 @@
-const arr = ["kutya","cica", "mérési hiba"]
-
-for(let i = 0; i < arr.length; i++){
-console.log(arr[i])
-}
---
+++ team.js
@@ -0,0 +1,7 @@
+let nouns = ["Actor", "Boy", "Cinnamon", "Dream", "Eagle", "Flower", "Gold", "Horizon", "Iron", "June", "Key", "Lemonade", "Melodies", "Notebook", "Ocean", "Painting", "Question", "Rat", "Shadow", "Toast", "Unicorn", "Vehicle", "Week", "Xylophone", "Year", "Zebra"]
+
+let adverbs = ["Abstractly", "Boldly", "Calmly", "Deeply", "Extremely", "Fearlessly", "Gracefully", "Happily", "Impulsively", "Jealously", "Kissably", "Lazily", "Mildly", "Nicely", "Oddly", "Prissily", "Quickly", "Relaxingly", "Slowly", "Tenderly", "Ultimately", "Very", "Warely", "Xylophonically", "Yearnfully", "Zanily"]
+
+let verbs = ["Adapting", "Bringing", "Crying", "Debunking", "Eating", "Feeling", "Growing", "Helping", "Inserting", "Jumping", "Kicking", "Learning", "Making", "Navigating", "Operating", "Pouring", "Running", "Sleeping", "Transforming", "Using", "Visiting", "Whirling", "Yawning", "Zooming"]
+
+let adjectives = ["Adorable", "Bright", "Charming", "Dangerous", "Emotional", "Fabulous", "Good", "Heavy", "Imaginary", "Joyful", "Kind", "Lazy", "Majestic", "Nice", "Optimal", "Proud", "Quick", "Real", "Strong", "Thunderous", "Understanding", "Villainous", "Weepy", "Xiphoid", "Yellow", "Zingy"]
