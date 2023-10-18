15b8884c55bc501ff8dbbe7aeabc49f915ea3b82
kistakki <takacspetra95@gmail.com>
2023-10-18 12:21:56 +0200

    partofwork

--- team.js
+++ team.js
@@ -2,6 +2,26 @@ let nouns = ["Actor", "Boy", "Cinnamon", "Dream", "Eagle", "Flower", "Gold", "Ho
 
 let adverbs = ["Abstractly", "Boldly", "Calmly", "Deeply", "Extremely", "Fearlessly", "Gracefully", "Happily", "Impulsively", "Jealously", "Kissably", "Lazily", "Mildly", "Nicely", "Oddly", "Prissily", "Quickly", "Relaxingly", "Slowly", "Tenderly", "Ultimately", "Very", "Warely", "Xylophonically", "Yearnfully", "Zanily"]
 
-let verbs = ["Adapting", "Bringing", "Crying", "Debunking", "Eating", "Feeling", "Growing", "Helping", "Inserting", "Jumping", "Kicking", "Learning", "Making", "Navigating", "Operating", "Pouring", "Running", "Sleeping", "Transforming", "Using", "Visiting", "Whirling", "Yawning", "Zooming"]
+let verbs = ["Adapting", "Bringing", "Crying", "Debunking", "Eating", "Feeling", "Growing", "Helping", "Inserting", "Jumping", "Kicking", "Learning", "Making", "Navigating", "Operating", "Pouring", "Qualiflying", "Running", "Sleeping", "Transforming", "Using", "Visiting", "Whirling", "Xenomorphing", "Yawning", "Zooming"]
 
 let adjectives = ["Adorable", "Bright", "Charming", "Dangerous", "Emotional", "Fabulous", "Good", "Heavy", "Imaginary", "Joyful", "Kind", "Lazy", "Majestic", "Nice", "Optimal", "Proud", "Quick", "Real", "Strong", "Thunderous", "Understanding", "Villainous", "Weepy", "Xiphoid", "Yellow", "Zingy"]
+
+function randomBandNameGenerator(baseWord) {
+    let result = "";
+    for(let nounList of nouns){
+        if(nounList[0].toLowerCase() === baseWord[0]){
+            result = nounList
+        }
+    }
+    for(let nounList of nouns){
+        if(nounList[0].toLowerCase() === baseWord[baseWord.length-1]){
+            result += nounList
+        }
+    }
+    return result
+    }
+    
+    console.log(randomBandNameGenerator("bohóc"))
+
+
+
